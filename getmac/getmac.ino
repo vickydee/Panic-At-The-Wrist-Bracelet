9b5688388cc69ee77fc510224439929540bce705
vickydee <vicfan006@gmail.com>
2026-04-18 10:54:45 -0700

    modify to work esp32

--- getmac/getmac.ino
+++ getmac/getmac.ino
@@ -1,26 +1,25 @@
 #include <WiFi.h>
 
-void setup() {
-  Serial.begin(115200);
-  delay(2000);  // Give Serial Monitor time to connect
+void printMacAddress() {
+  uint8_t baseMac[6];
+  // Get the base MAC address
+  esp_read_mac(baseMac, ESP_MAC_WIFI_STA);
   
-  Serial.println("\n\n===== ESP32-S3 MAC Address Reader =====");
-  
-  // Method 1: Get MAC as formatted string (simplest)
-  Serial.print("MAC Address (String): ");
-  Serial.println(WiFi.macAddress());
-  
-  // Method 2: Get MAC as byte array
-  uint8_t mac[6];
-  WiFi.macAddress(mac);
-  
-  Serial.print("MAC Address (Hex): ");
-  for (int i = 0; i < 6; i++) {
-    if (mac[i] < 16) Serial.print("0");
-    Serial.print(mac[i], HEX);
-    if (i < 5) Serial.print(":");
+  Serial.print("MAC Address: ");
+  for (int i = 0; i < 6; ++i) {
+    Serial.printf("%02x", baseMac[i]);
+    if (i < 5)
+      Serial.print(":");
   }
   Serial.println();
+}
+
+void setup() {
+  Serial.begin(115200);
+  delay(2000);
+  
+  Serial.println("\n\n===== ESP32 MAC Address Reader =====");
+  printMacAddress();
 }
 
 void loop() {
