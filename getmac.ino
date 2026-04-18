497e00faec9ce3f309777e7b9395df240f1b0dc0
vickydee <vicfan006@gmail.com>
2026-04-18 10:42:49 -0700

    update code and name

--- getmac.ino
+++ getmac.ino
@@ -4,23 +4,21 @@ void setup() {
   Serial.begin(115200);
   delay(1000);
   
-  // Get MAC address in different formats
+  // Method 1: Get MAC as formatted string (simplest)
+  Serial.print("MAC Address (String): ");
+  Serial.println(WiFi.macAddress());
   
-  // Format 1: As a byte array
+  // Method 2: Get MAC as byte array
   uint8_t mac[6];
-  esp_read_mac(mac, ESP_MAC_WIFI_STA);
+  WiFi.macAddress(mac);
   
-  Serial.print("MAC Address (hex): ");
+  Serial.print("MAC Address (Hex): ");
   for (int i = 0; i < 6; i++) {
     if (mac[i] < 16) Serial.print("0");
     Serial.print(mac[i], HEX);
     if (i < 5) Serial.print(":");
   }
   Serial.println();
-  
-  // Format 2: Using WiFi.macAddress() - easier method
-  Serial.print("MAC Address (WiFi): ");
-  Serial.println(WiFi.macAddress());
 }
 
 void loop() {
