e22c68124447625393a1678d4c537573e98e973d
vickydee <vicfan006@gmail.com>
2026-04-18 12:51:08 -0700

    created working mac address getter for esp32d

--- getmac/getmac.ino
+++ getmac/getmac.ino
@@ -1,23 +1,65 @@
 #include <WiFi.h>
+#include <esp_system.h>
+
+void printMacAddresses() {
+  uint64_t chipId = ESP.getEfuseMac();
+  uint8_t baseMac[6];
+
+  baseMac[0] = (chipId >> 40) & 0xFF;
+  baseMac[1] = (chipId >> 32) & 0xFF;
+  baseMac[2] = (chipId >> 24) & 0xFF;
+  baseMac[3] = (chipId >> 16) & 0xFF;
+  baseMac[4] = (chipId >> 8) & 0xFF;
+  baseMac[5] = chipId & 0xFF;
+
+  WiFi.mode(WIFI_STA);
+  delay(100);
+
+  Serial.println();
+  Serial.println("ESP32-D MAC address reader");
+  Serial.println("--------------------------");
+
+  Serial.print("WiFi STA MAC: ");
+  Serial.println(WiFi.macAddress());
+
+  Serial.print("Base MAC:     ");
+  for (int i = 0; i < 6; i++) {
+    if (baseMac[i] < 16) {
+      Serial.print("0");
+    }
+    Serial.print(baseMac[i], HEX);
+    if (i < 5) {
+      Serial.print(":");
+    }
+  }
+  Serial.println();
+  Serial.println();
+  Serial.println("Type mac and press Enter to print it again.");
+}
 
 void setup() {
   Serial.begin(115200);
-  delay(1000);
-  
-  Serial.println("");
-  Serial.println("ESP32 MAC Address");
-  Serial.println("=================");
-  
-  // Get MAC address using WiFi library (works on all ESP32 variants)
-  WiFi.mode(WIFI_STA);
-  delay(100);
-  
-  Serial.print("MAC: ");
-  Serial.println(WiFi.macAddress());
-  
-  Serial.println("Done");
+  delay(2000);
+  printMacAddresses();
 }
 
 void loop() {
-  delay(10000);
+  static unsigned long lastReminder = 0;
+
+  if (Serial.available()) {
+    String command = Serial.readStringUntil('\n');
+    command.trim();
+    command.toLowerCase();
+
+    if (command == "mac") {
+      printMacAddresses();
+    } else if (command.length() > 0) {
+      Serial.println("Type mac and press Enter.");
+    }
+  }
+
+  if (millis() - lastReminder >= 5000) {
+    lastReminder = millis();
+    Serial.println("Waiting... type mac and press Enter.");
+  }
 }
